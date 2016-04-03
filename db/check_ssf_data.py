6f4b0eabe855f46151992413854fd5a79bab92ed
pdh <himtemp@gmail.com>
2016-04-03 23:40:26 +0700

    added ValueError error handling
    version 0.77

--- db/check_ssf_data.py
+++ db/check_ssf_data.py
@@ -1,11 +1,12 @@
 import insert_data
 from read_transactions_from_excel import TickDataRows
 import fileUtils
+import sys
 import datetime
 
 test_folder = 'C:\\Users\\Peerapong\\Documents\\market data\\lot 20141209\\SSF\\ADVANC\\'
 test_folder2 = 'C:\\Users\\Peerapong\\Documents\\market data\\lot 20141209\\SSF\\SPALI\\'
-
+base_folder = 'C:\\Users\\Peerapong\\Documents\\market data\\lot 20141209\\SSF\\'
 
 def check_ssf_data(fileList, outFileName):
     out = open(outFileName, 'wt', encoding="utf8")
@@ -16,20 +17,24 @@ def check_ssf_data(fileList, outFileName):
             raise ValueError
         fname = path_tokens[len(path_tokens)-1]
         print(fname)
-        values, col_str, format_str, con_symbol = insert_data.get_ssf_metadata_string_and_values(fname, "", "NULL", "NULL")
-        
-        num_rows = 0
-        for row in TickDataRows(filename):
-            num_rows = num_rows + 1
-            result = [col.value for col in row]
-            isValid, errorCode = insert_data.validate_ssf_data(filename, result)
-            if isValid:
-                pass
-            else:
-                out.write(filename + "," + str(errorCode+10) + "," + str(result[0]))
-            break;
-        if num_rows == 0:
-            out.write(filename + ",1,")
+        try:
+            values, col_str, format_str, con_symbol = insert_data.get_ssf_metadata_string_and_values(fname, "", "NULL", "NULL")
+            
+            num_rows = 0
+            for row in TickDataRows(filename):
+                num_rows = num_rows + 1
+                result = [col.value for col in row]
+                isValid, errorCode = insert_data.validate_ssf_data(filename, result)
+                if isValid:
+                    pass
+                else:
+                    out.write(filename + "," + str(errorCode+10) + "," + str(result[0]) + "\n")
+                break;
+            if num_rows == 0:
+                out.write(filename + ",1,\n")
+        except ValueError as e:
+            tmp = sys.exc_info()[:2]
+            out.write(filename + ",22,ValueError,{0}\n".format(tmp[1]))
     out.close()
 
 
